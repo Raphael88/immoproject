45e07fe5d876429f5f6dfde5a96d18b3668a1fb7
Raphael88 <raphael.nguyenvan@gmail.com>
2025-06-03 21:45:48 +0200

    Chamge number of supplied parameters

--- API_immoproject.py
+++ API_immoproject.py
@@ -317,7 +317,7 @@ def now_online():
         conn = pyodbc.connect(connection_string)
         cursor = conn.cursor()
 
-        query = "SELECT * FROM desc_ad as a LEFT JOIN market as b ON b.market_name = a.place WHERE b.Id = ? AND a.type_bien = ? AND a.nomb_piece = ?  AND (a.terr_m2 > ? AND a.terr_m2 < ?)"
+        query = "SELECT * FROM desc_ad as a LEFT JOIN market as b ON b.market_name = a.place WHERE b.Id = ? AND a.type_bien = ? AND a.nomb_piece = ? AND (a.hab_m2 > ? AND a.hab_m2 < ?)  AND (a.terr_m2 > ? AND a.terr_m2 < ?)"
         cursor.execute(query, (f1,f2, f3,  f5_inf, f5_sup,f4_inf,f4_sup,))
 
         columns = [column[0] for column in cursor.description]
